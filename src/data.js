117b3eb902234143f8e379fb5f984d61a2b513f9
shekarramaswamy4 <shekar.ram.428@gmail.com>
2018-11-27 19:33:41 -0500

    added dropdowns

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 export const data = [
-    { name: "NMD", brand: "Adidas", color: "white", price: 130, 
+    { name: "NMD", brand: "Adidas", color: "White", price: 130, 
     img: "https://assets.adidas.com/images/w_840,h_840,f_auto,q_90,fl_lossy/6e0d2178b38f4c62b50ba93700e95204_9366/NMD_R1_Shoes_White_G27938_01_standard.jpg"},
-    { name: "A", brand: "Adidas", color: "black", price: 160, 
+    { name: "A", brand: "Adidas", color: "Black", price: 160, 
     img: "https://assets.adidas.com/images/w_840,h_840,f_auto,q_90,fl_lossy/6e0d2178b38f4c62b50ba93700e95204_9366/NMD_R1_Shoes_White_G27938_01_standard.jpg"},
-    { name: "B", brand: "Adidas", color: "white", price: 120, 
+    { name: "B", brand: "Adidas", color: "White", price: 120, 
     img: "https://assets.adidas.com/images/w_840,h_840,f_auto,q_90,fl_lossy/6e0d2178b38f4c62b50ba93700e95204_9366/NMD_R1_Shoes_White_G27938_01_standard.jpg"},
-    { name: "C", brand: "Nike", color: "red", price: 200, 
+    { name: "C", brand: "Nike", color: "Red", price: 200, 
     img: "https://assets.adidas.com/images/w_840,h_840,f_auto,q_90,fl_lossy/6e0d2178b38f4c62b50ba93700e95204_9366/NMD_R1_Shoes_White_G27938_01_standard.jpg"},
-    { name: "D", brand: "Puma", color: "white", price: 80, 
+    { name: "D", brand: "Puma", color: "White", price: 80, 
     img: "https://assets.adidas.com/images/w_840,h_840,f_auto,q_90,fl_lossy/6e0d2178b38f4c62b50ba93700e95204_9366/NMD_R1_Shoes_White_G27938_01_standard.jpg"},
 ]
--- src/list/FilteredList.jsx
+++ src/list/FilteredList.jsx
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import List from './List';
-import { DropdownButton, MenuItem} from 'react-bootstrap';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 /*
  * Handles filtering logic for items. Supports brand and color filtering,
  *   and price sorting. 
 */
-class FilteredList extends Component {
+class FilteredList extends Component { 
   constructor(props) {
     super(props);
     this.state = {
-      brand: "",
-      color: "",
-      sorted: "des"
+      brand: "All",
+      color: "All",
+      sorted: ""
     };
 }
 
-// handleDropDown = (newType) => {
-//   this.setState({type: newType})
-// }
+dropDownSelect = (newFilter, field) => {
+  if (field === 'brand') {
+    this.setState({ brand: newFilter })
+  } else if (field === 'color') {
+    this.setState({ color: newFilter })
+  } else if (field === 'sorted') {
+    this.setState({ sorted: newFilter })
+  }
+}
 
 handleBrandFilter = (item) => {
-    return (this.state.brand === "" || this.state.brand === item.brand);
+    return (this.state.brand === "All" || this.state.brand === item.brand);
 }
 
 handleColorFilter = (item) => {
-    return (this.state.color === "" || this.state.color === item.color);
+    return (this.state.color === "All" || this.state.color === item.color);
 }
 
 filterItem = (item) => {
@@ -44,17 +50,24 @@ sortItems = (items) => {
 }
 
 reset = () => {
-    this.setState({ brand: "", color: "", sorted: "" })
+    this.setState({ brand: "All", color: "All", sorted: "" })
 }
 
 render() {
   return (
     <div className="filter-list">
-      {/* <h1>Produce Search</h1>
-      <DropdownButton id="typeDropdown" title={this.state.type}>
-        <MenuItem eventKey="All" onSelect={() => this.handleDropDown('All')}>All</MenuItem>
-        <MenuItem eventKey="Fruit" onSelect={() => this.handleDropDown('Fruit')}>Fruits</MenuItem>
-        <MenuItem eventKey="Vegetables" onSelect={() => this.handleDropDown('Vegetable')}>Vegetables</MenuItem>
+      <DropdownButton id="brandDropdown" title={this.state.brand}>
+        <MenuItem eventKey="All" onSelect={() => this.dropDownSelect('All', 'brand')}>All</MenuItem>
+        <MenuItem eventKey="Nike" onSelect={() => this.dropDownSelect('Nike', 'brand')}>Nike</MenuItem>
+        <MenuItem eventKey="Adidas" onSelect={() => this.dropDownSelect('Adidas', 'brand')}>Adidas</MenuItem>
+        <MenuItem eventKey="Puma" onSelect={() => this.dropDownSelect('Puma', 'brand')}>Puma</MenuItem>
+      </DropdownButton>
+
+      <DropdownButton id="colorDropdown" title={this.state.color}>
+        <MenuItem eventKey="All" onSelect={() => this.dropDownSelect('All', 'color')}>All</MenuItem>
+        <MenuItem eventKey="White" onSelect={() => this.dropDownSelect('White', 'color')}>White</MenuItem>
+        <MenuItem eventKey="Black" onSelect={() => this.dropDownSelect('Black', 'color')}>Black</MenuItem>
+        <MenuItem eventKey="Red" onSelect={() => this.dropDownSelect('Red', 'color')}>Red</MenuItem>
       </DropdownButton>
 
         {/*
