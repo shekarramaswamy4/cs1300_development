2071c2d219cbab161391013f34dd37154310a536
shekarramaswamy4 <shekar.ram.428@gmail.com>
2018-12-04 17:33:03 -0500

    fixed links

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -10,12 +10,13 @@ class Card extends Component {
   render() {
     const item = this.props.data;
 
+    let link = "https://www.google.com/search?q=" + item.name
     // return statement hides the card if its name field is hidden (for spacing)
     return (
       <div className="card-wrapper">
         <Thumbnail src={item.img} className="image"
           style={item.name === "hidden" ? { visibility: "hidden" } : {}}>
-          <a href="https://www.google.com">
+          <a href={link} target="_blank">
           < h3>{item.name}</h3>
           </a>
             <br /> <br />
